4429a99b15df226a021a7b9f8220f0bc8a3a1914
dhirajnashtech <dhiraj.sonawane@nashtechglobal.com>
2025-09-23 18:24:34 +0530

    adding app code with secret

---
+++ app.py
@@ -0,0 +1,14 @@
+# app.py
+
+import streamlit as st
+
+def main():
+    st.title("Gitleaks Test App")
+    # Hardcoded secret for testing Gitleaks scanning
+    SECRET_API_KEY = "REMOVED"
+    st.write("This is a sample Streamlit app.")
+    st.write("Your secret API key is:", SECRET_API_KEY)
+
+if __name__ == "__main__":
+    main()
+
