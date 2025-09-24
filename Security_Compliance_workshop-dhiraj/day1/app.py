a68c798f8f63f8a585f1153e72f0b8cbf5cd894a
dhirajnashtech <129745737+dhirajnashtech@users.noreply.github.com>
2025-09-24 12:23:07 +0530

    Update app.py

--- Security_Compliance_workshop-dhiraj/day1/app.py
+++ Security_Compliance_workshop-dhiraj/day1/app.py
@@ -1,14 +1,17 @@
 # app.py
 
-import streamlit as st
+from flask import Flask
+import os
 
-def main():
-    st.title("Gitleaks Test App")
-    # Hardcoded secret for testing Gitleaks scanning
+app = Flask(__name__)
+
 #    SECRET_API_KEY = "REMOVED"
-    st.write("This is a sample Streamlit app.")
-    st.write("Your secret API key is:", SECRET_API_KEY)
+app.config['SECRET_KEY'] = 'SECRET_API_KEY'
 
-if __name__ == "__main__":
-    main()
+@app.route('/')
+def home():
+    return "Gitleaks implimentation working!"
 
+if __name__ == '__main__':
+    # Port and host configuration for Docker
+    app.run(host='0.0.0.0', port=8080)
